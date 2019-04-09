187e940e59606f8d33acfa8f02ea3ace331ccdc8
Rui Gomes <hello@ruigomes.me>
2019-04-09 12:55:10 +0100

    Checking payments

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const axios = require('axios');
 const db = require('./db.js');
 
 const opennodeKey = '95164e77-7feb-43f1-9fb7-f5c1149d84dc';
+const replUrl = 'https://opennode-workshop--ruigomes.repl.co';
 
 app.get('/', (req, res) => {
-  db.getAllMessages().then(messages => {
+  db.getAllPaidMessages().then(messages => {
     return res.render('index.ejs', { messages });
   });
 });
@@ -23,11 +24,26 @@ app.post('/messages', async (req, res) => {
     amount: 1,
     description: 'New message',
     order_id: id,
+    callback_url: replUrl + '/webhook'
   }, { headers: { Authorization: opennodeKey } })
     .then(response => {
       res.render('payment.ejs', { payreq: response.data.data.lightning_invoice.payreq });
     })
     .catch(error => console.log(error.response.data));
+});
+
+app.post('/webhook', (req, res) => {
+  console.log('OpenNode Webhook', req.body);
+
+  const status = req.body.status;
+
+  if(status !== 'paid') {
+    return res.send('Order not paid');
+  }
+
+  db.markMessageAsPaid(req.body.order_id);
+
+  return res.send('Order paid');
 });
 
 app.listen(3000, () => console.log(`App listening on port 3000!`));
