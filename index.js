af58dae5c7dfa54bfadaee27e431bc377eb82662
Rui Gomes <hello@ruigomes.me>
2019-04-09 12:53:29 +0100

    Creating invoices

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const app = require('./app.js');
 const axios = require('axios');
 const db = require('./db.js');
 
+const opennodeKey = '95164e77-7feb-43f1-9fb7-f5c1149d84dc';
+
 app.get('/', (req, res) => {
   db.getAllMessages().then(messages => {
-    console.log(messages);
     return res.render('index.ejs', { messages });
   });
 });
@@ -18,7 +19,15 @@ app.post('/messages', async (req, res) => {
     content, paid, createdAt
   });
 
-  res.redirect('/');
+  axios.post('https://dev-api.opennode.co/v1/charges', {
+    amount: 1,
+    description: 'New message',
+    order_id: id,
+  }, { headers: { Authorization: opennodeKey } })
+    .then(response => {
+      res.render('payment.ejs', { payreq: response.data.data.lightning_invoice.payreq });
+    })
+    .catch(error => console.log(error.response.data));
 });
 
 app.listen(3000, () => console.log(`App listening on port 3000!`));
